4e9e5ba4043a32271b8c42aedc705ff995e1ac69
Sebastien Mesili <sebastien@terafunding.com>
2018-11-19 18:33:23 +0900

    Added Seoul & South Korea Maps. eslint --fix not auto fixing the indent.

---
+++ examples/src/components/south-korea.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { SVGMap, SouthKorea } from '../../../src/';
+import { getLocationId, getLocationName } from '../utils';
+import '../../../src/svg-map.scss';
+
+class SouthKoreaMap extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pointedLocation: null,
+            focusedLocation: null,
+            clickedLocation: null
+        };
+
+        this.handleLocationMouseOver = this.handleLocationMouseOver.bind(this);
+        this.handleLocationMouseOut = this.handleLocationMouseOut.bind(this);
+        this.handleLocationClick = this.handleLocationClick.bind(this);
+        this.handleLocationFocus = this.handleLocationFocus.bind(this);
+        this.handleLocationBlur = this.handleLocationBlur.bind(this);
+    }
+
+    handleLocationMouseOver(event) {
+        const pointedLocation = getLocationName(event);
+        this.setState({ pointedLocation: pointedLocation });
+    }
+
+    handleLocationMouseOut() {
+        this.setState({ pointedLocation: null });
+    }
+
+    handleLocationClick(event) {
+        const clickedLocation = getLocationName(event);
+        const clickedLocationId = getLocationId(event);
+        this.setState({ clickedLocation: clickedLocation });
+    }
+
+    handleLocationFocus(event) {
+        const focusedLocation = getLocationName(event);
+        this.setState({ focusedLocation: focusedLocation });
+    }
+
+    handleLocationBlur() {
+        this.setState({ focusedLocation: null });
+    }
+
+    render() {
+        return (
+            <article className="examples__block">
+                <h2 className="examples__block__title">
+                    South Korea SVG map
+                </h2>
+                <div className="examples__block__info">
+                    <div className="examples__block__info__item">
+                        Pointed location: {this.state.pointedLocation}
+                    </div>
+                    <div className="examples__block__info__item">
+                        Focused location: {this.state.focusedLocation}
+                    </div>
+                    <div className="examples__block__info__item">
+                        Clicked location: {this.state.clickedLocation}
+                    </div>
+                </div>
+                <div className="examples__block__map">
+                    <SVGMap
+                        map={SouthKorea}
+                        type="link"
+                        onLocationMouseOver={this.handleLocationMouseOver}
+                        onLocationMouseOut={this.handleLocationMouseOut}
+                        onLocationClick={this.handleLocationClick}
+                        onLocationFocus={this.handleLocationFocus}
+                        onLocationBlur={this.handleLocationBlur} />
+                </div>
+            </article>
+        );
+    }
+}
+
+export default SouthKoreaMap;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import SVGMap from './svg-map';
 import Australia from './maps/australia';
 import France from './maps/france';
+import SouthKorea from './maps/south-korea';
+import Seoul from './maps/seoul';
 import Taiwan from './maps/taiwan';
 import USA from './maps/usa';
 import Utah from './maps/utah';
@@ -11,6 +13,8 @@ export {
 	Australia,
 	France,
 	Taiwan,
+	SouthKorea,
+	Seoul,
 	USA,
 	Utah
 };
